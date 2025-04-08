1825e5b135e42cbd0806a19262e2b786d8648c90
Rdrudra99 <rdrudra99.work@gmail.com>
2025-04-08 18:34:50 +0530

    Remove Husky from package.json dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,21 +45,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png" />
-        <meta name="theme-color" content="#e0c8fd" />
-
-        <meta property="og:title" content="Nextjs App" />
-        <meta name="twitter:title" content="Nextjs App" />
-
-        <meta name="description" content="Nextjs 14 starter template" />
-        <meta property="og:description" content="Nextjs 14 starter template" />
-        <meta name="twitter:description" content="Nextjs 14 starter template" />
-
-        <meta property="og:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
-        <meta name="twitter:image" content="https://i.imgur.com/Z3bMJXy.jpg" />
-      </head>
       {/* //! If you don't want 'screen size' visible at the left bottom of the browser window, You can remove `debug-screens` class */}
       <body
         className={classNames(`${inter.className} antialiased`, {
